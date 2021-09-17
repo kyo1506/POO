bee25e56b0e6d77f0ffe6d3c65bbd738951015a0
kyo1506 <vinicius_pinheiro02@hotmail.com>
2021-09-16 22:01:50 -0300

    LP07

--- LP07/src/main/java/Main.java
+++ LP07/src/main/java/Main.java
@@ -5,15 +5,19 @@ public class Main {
         Superior superior = new Superior();
         Tecnico tecnico = new Tecnico();
 
-        superior.setN1(Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor da P1","")));
-        superior.setN2(Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor da P2","")));
-        superior.setN3(Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor da P3","")));
+        try {
+            superior.setN1(Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor da P1", "")));
+            superior.setN2(Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor da P2", "")));
+            superior.setN3(Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor da P3", "")));
 
-        tecnico.setN1(Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor da nota do 1° Trimestre","")));
-        tecnico.setN2(Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor da nota do 1° Trimestre","")));
-        tecnico.setN3(Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor da nota do 1° Trimestre","")));
+            tecnico.setN1(Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor da nota do 1° Trimestre", "")));
+            tecnico.setN2(Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor da nota do 1° Trimestre", "")));
+            tecnico.setN3(Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor da nota do 1° Trimestre", "")));
 
-        JOptionPane.showMessageDialog(null,"A média das notas de P1: " + superior.getN1() + ", P2: " + superior.getN2() + " e Trabalhos: " + superior.getN3() + " é " + superior.CalculaMedia() + ".", "Information", JOptionPane.INFORMATION_MESSAGE);
-        JOptionPane.showMessageDialog(null,"A média das notas do 1° Trimestre: " + tecnico.getN1() + ", 2° Trimestre: " + tecnico.getN2() + " e 3° Trimestre: " + tecnico.getN3() + " é " + tecnico.CalculaMedia() + ".", "Information", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "A média das notas de P1: " + superior.getN1() + ", P2: " + superior.getN2() + " e Trabalhos: " + superior.getN3() + " é " + superior.CalculaMedia() + ".", "Information", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "A média das notas do 1° Trimestre: " + tecnico.getN1() + ", 2° Trimestre: " + tecnico.getN2() + " e 3° Trimestre: " + tecnico.getN3() + " é " + tecnico.CalculaMedia() + ".", "Information", JOptionPane.INFORMATION_MESSAGE);
+        }catch(Exception ex){
+            JOptionPane.showMessageDialog(null, ex.getMessage(),"Error",JOptionPane.ERROR_MESSAGE);
+        }
     }
 }
