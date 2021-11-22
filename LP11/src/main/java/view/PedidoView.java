0f2e00715c8903b4507f0659ec93b7155fbb2dbd
kyo1506 <vinicius_pinheiro02@hotmail.com>
2021-11-22 20:46:28 -0300

    LP11 - PedidoView

---
+++ LP11/src/main/java/view/PedidoView.java
@@ -0,0 +1,10 @@
+package view;
+
+import javax.swing.*;
+
+public class PedidoView {
+    private JPanel mainPanel;
+    private JButton btnNovoPedido;
+    private JButton btnMeusPedidos;
+    private JButton btnFechar;
+}
